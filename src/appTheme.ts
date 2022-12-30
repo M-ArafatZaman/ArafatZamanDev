6ac4d6ba1f74aceaaf12588bcc2f8c6b38b7c74c
M-ArafatZaman <mdarafatzaman1@gmail.com>
2022-12-30 18:43:45 -0400

    Added app theme

---
+++ src/appTheme.ts
@@ -0,0 +1,14 @@
+import {createTheme} from '@mui/material/styles';
+
+const APP_THEME = createTheme({
+    palette: {
+        primary: {
+            main: "#14EADD"
+        },
+        secondary: {
+            main: "#09605A"
+        }
+    }
+})
+
+export {APP_THEME};
