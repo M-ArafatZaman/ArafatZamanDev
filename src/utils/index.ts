b78edb75c859effd0f8dfc5acf57d21de35b2f55
M-ArafatZaman <mdarafatzaman1@gmail.com>
2023-01-22 15:13:03 -0330

    Added a component that performs an animation of an element once the element is in viewport

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,14 @@
 import toHex from './toHex';
 import LazyImport from './LazyImport';
 import FadeInWrapper from './FadeInWrapper';
+import ElementInViewport from './ElementInViewport';
+import {isElementInViewport, isAnyPartOfElementInViewport} from './viewport';
 
 export {
     toHex,
     LazyImport,
-    FadeInWrapper
+    FadeInWrapper,
+    ElementInViewport,
+    isElementInViewport,
+    isAnyPartOfElementInViewport
 };
