9e0ff35261b418f59a8ce2ba8e103872272a126b
M-ArafatZaman <mdarafatzaman1@gmail.com>
2023-01-22 16:19:17 -0330

    Made the header sticky

--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -1,9 +1,6 @@
-import React from 'react';
+import React, {createRef, useEffect} from 'react';
 // @mui components
 import {
-    AppBar,
-    Toolbar,
-    Typography,
     Container,
     Box,
     useTheme
@@ -27,23 +24,65 @@ const Header: React.FC = () => {
         padding: 1
     };
 
-    return (
-        /* Header container for the padding */
-        <Box sx={{padding: '8px', pb: 0}}>
-            {/* The header */}
-            <Box sx={headerStyles}>
-                <Container>
-                    <Box display="flex" flexDirection="row">
-                        {/* The logo */}
-                        <Box display="flex" flexGrow={1}>
-                            <img src={LOGO} style={{height: 50, width: 50}} />
-                        </Box>
+    // Header container styles
+    const headerContainerStyles: SxProps = {
+        position: "fixed",
+        zIndex: 1000,
+        top: 0, left: 0, right: 0,
+        padding: "8px",
+        pb: 0,
+        backdropFilter: "blur(16px)"
+    };
 
-                        {/* Nav bar */}
-                        <Navbar/>
-                    </Box>
-                </Container>
+    // Header ref and header backdrop ref
+    const HeaderRef: React.RefObject<HTMLElement> = createRef<HTMLElement>();
+    const HeaderBackdropRef: React.RefObject<HTMLDivElement> = createRef<HTMLDivElement>();
+
+    // Add event listener so that the backdrop mimics the height of the header
+    useEffect(() => {
+        const onResize = () => {
+            if (
+                HeaderRef.current instanceof HTMLElement && 
+                HeaderBackdropRef.current instanceof HTMLDivElement && 
+                typeof HeaderRef.current !== "undefined" &&
+                typeof HeaderBackdropRef.current !== "undefined"
+            ) {
+                HeaderBackdropRef.current.style.height = `${HeaderRef.current.clientHeight}px`;
+            }
+        };
+        // Execute it once
+        onResize();
+
+        window.addEventListener("resize", onResize);
+
+        // Remove event listener on destructor
+        return () => {
+            window.removeEventListener("resize", onResize);
+        }
+    }, [])
+
+    return (
+        <Box sx={{position: "relative"}}>
+            {/* Header container for the padding */}
+            <Box sx={headerContainerStyles}>
+                {/* The header */}
+                <Box sx={headerStyles} ref={HeaderRef}>
+                    <Container>
+                        <Box display="flex" flexDirection="row">
+                            {/* The logo */}
+                            <Box display="flex" flexGrow={1}>
+                                <img src={LOGO} style={{height: 50, width: 50}} />
+                            </Box>
+
+                            {/* Nav bar */}
+                            <Navbar/>
+                        </Box>
+                    </Container>
+                </Box>
             </Box>
+            
+            {/* A header backdrop */}
+            <div ref={HeaderBackdropRef} style={{width: "100%"}}></div>
         </Box>
     );
 };
