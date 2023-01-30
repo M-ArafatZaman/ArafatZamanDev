22eaf25dcc2e503babe3c2e800392bd8364f61a4
M-ArafatZaman <mdarafatzaman1@gmail.com>
2023-01-30 16:36:00 -0330

    Added a menu toggle button for the viewportfolionavbar

--- src/pages/portfolio/ViewPortfolioNavbar.tsx
+++ src/pages/portfolio/ViewPortfolioNavbar.tsx
@@ -1,10 +1,14 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import {useNavigate, useParams} from 'react-router-dom';
 // @mui components
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import Collapse from '@mui/material/Collapse';
+// @mui icons
+import MenuIcon from '@mui/icons-material/Menu';
 // Other components
 import AppCard from '../home/components/AppCard';
 // App theme
@@ -14,26 +18,38 @@ import {PortfolioContext} from './portfolioContext';
 
 const ViewPortfolioNavbar: React.FC = () => {
 
+    const [collapsed, setCollapsed] = useState<boolean>(false);
     const params = useParams<{slug: string}>();
     const PContext = useContext(PortfolioContext);
     const navigate = useNavigate();
 
+    // When the menu button is clicked, toggle the buttons
+    const toggleButton = () => {
+        setCollapsed((prev) => !prev);
+    };
+
     return (
         <AppCard sx={{backgroundColor: APP_THEME.palette.primary.main}}>
-            <Box p={2}>
+            <Box p={2} display="flex" alignItems="center">
+                <IconButton onClick={toggleButton}><MenuIcon/></IconButton>
                 <Typography variant="h6">Contents</Typography>
-                <Divider sx={{my: 1}}/>
-                {PContext.items.map((P_ITEMS, i) => (
-                    <Button 
-                        key={i}
-                        fullWidth 
-                        variant="contained" 
-                        sx={{backgroundColor: "rgba(255,255,255,0.4)", my: 0.5}}
-                        onClick={() => {navigate(`/portfolio/${P_ITEMS.slug}`)}}
-                        disabled={params.slug == P_ITEMS.slug}
-                    >{P_ITEMS.name}</Button>
-                ))}
             </Box>
+
+            {/* Buttons */}
+            <Collapse in={!collapsed}>
+                <Box p={2} sx={{borderTop: "1px solid rgba(0,0,0,.15)"}}>
+                    {PContext.items.map((P_ITEMS, i) => (
+                        <Button 
+                            key={i}
+                            fullWidth 
+                            variant="contained" 
+                            sx={{backgroundColor: "rgba(255,255,255,0.4)", my: 0.5}}
+                            onClick={() => {navigate(`/portfolio/${P_ITEMS.slug}`)}}
+                            disabled={params.slug == P_ITEMS.slug}
+                        >{P_ITEMS.name}</Button>
+                    ))}
+                </Box>
+            </Collapse>
         </AppCard>
     )
 };
