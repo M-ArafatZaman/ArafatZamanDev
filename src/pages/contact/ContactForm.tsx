2fefd984cad66fdfdf9a523727653eaecc26cd85
M-ArafatZaman <mdarafatzaman1@gmail.com>
2023-02-07 19:06:25 -0330

    Button is only valid when inputs are valid

--- src/pages/contact/ContactForm.tsx
+++ src/pages/contact/ContactForm.tsx
@@ -1,11 +1,12 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 // @mui components
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 // @mui icons
 import Icon from '@mui/material/Icon';
-import MailIcon from "@mui/icons-material/Mail";
+import SendIcon from '@mui/icons-material/Send';
 
 const TITLE_LENGTH = 255;
 const MESSAGE_LENGTH = 50;
@@ -14,6 +15,7 @@ const ContactForm: React.FC = () => {
 
     const [title, setTitle] = useState<string>("");
     const [message, setMessage] = useState<string>("");
+    const [valid, setValid] = useState<boolean>(false);
 
     // Function to update the title
     const UpdateTitle = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
@@ -24,6 +26,16 @@ const ContactForm: React.FC = () => {
     const UpdateMessage = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
         setMessage(e.currentTarget.value.substring(0, MESSAGE_LENGTH))
     };
+
+    useEffect(() => {
+        if (title.length === 0 || title.length > TITLE_LENGTH) {
+            setValid(false);
+        } else if (message.length === 0 || message.length > MESSAGE_LENGTH) {
+            setValid(false);
+        } else {
+            setValid(true);
+        }
+    }, [title, message]);
 
     return (
         <Box p={2}>
@@ -58,6 +70,13 @@ const ContactForm: React.FC = () => {
                     helperText={message.length === MESSAGE_LENGTH ? "Maxmium length reached" : ""}
                 />
             </Box>
+            <Button 
+                sx={{mt: 1}}
+                variant="contained"
+                color="warning"
+                startIcon={<SendIcon/>}
+                disabled={!valid}
+            >Send</Button>
         </Box>
     )
 };
