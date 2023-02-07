78c19c240f5a9fd1c2eab1d06a85522bd67a4757
M-ArafatZaman <mdarafatzaman1@gmail.com>
2023-02-07 19:54:47 -0330

    Added functionality to send message to server

--- src/pages/contact/ContactForm.tsx
+++ src/pages/contact/ContactForm.tsx
@@ -4,18 +4,52 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import Snackbar from '@mui/material/Snackbar';
+import Alert from '@mui/material/Alert';
+import IconButton from '@mui/material/IconButton';
 // @mui icons
 import Icon from '@mui/material/Icon';
 import SendIcon from '@mui/icons-material/Send';
+import CloseIcon from '@mui/icons-material/Close';
+// Endpoints
+import {BASE, SEND_MESSAGE} from './ENDPOINT';
 
 const TITLE_LENGTH = 255;
 const MESSAGE_LENGTH = 50;
+
+interface SnackDataInterface {
+    open: boolean;
+    severity: "error" | "success";
+    message: string;
+}
 
 const ContactForm: React.FC = () => {
 
     const [title, setTitle] = useState<string>("");
     const [message, setMessage] = useState<string>("");
     const [valid, setValid] = useState<boolean>(false);
+    // State of the snackbar
+    const [SnackData, setSnackData] = useState<SnackDataInterface>({
+        open: false, severity: "success", message: "NULL"
+    });
+
+    // Function to close the snackbar
+    const closeSnackbar = () => {
+        setSnackData((prev) => ({
+            ...prev,
+            open: false
+        }));
+    };
+
+    // Function to show error on snackbar
+    const snackError = () => {
+        setSnackData((prev) => ({
+            ...prev,
+            open: true,
+            severity: "error",
+            message: "An error occured."
+        }));
+    };
 
     // Function to update the title
     const UpdateTitle = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
@@ -37,7 +71,43 @@ const ContactForm: React.FC = () => {
         }
     }, [title, message]);
 
+    // Send message
+    const sendMessageToServer = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault();
+        
+        fetch(`${BASE}${SEND_MESSAGE}`, {
+            mode: "cors",
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                title: title,
+                message: message
+            })
+        })
+        .then((resp) => resp.json())
+        .then((resp) => {
+            if (resp.status === "OK") {
+                // Message was sent successfully
+                setSnackData({
+                    open: true,
+                    severity: "success",
+                    message: "Message was successfully sent!"
+                });
+            } else {
+                // else an error occurred
+                snackError();
+            }
+        })
+        .catch((e) => {
+            // An error occured
+            snackError();
+        })
+    };
+
     return (
+        <>
         <Box p={2}>
             <Typography variant="body2"><b>Send a message as anon</b></Typography>
             
@@ -76,8 +146,23 @@ const ContactForm: React.FC = () => {
                 color="warning"
                 startIcon={<SendIcon/>}
                 disabled={!valid}
+                onClick={sendMessageToServer}
             >Send</Button>
         </Box>
+        
+        {/* Snackbar to show status of the response */}
+        <Snackbar 
+            open={SnackData.open}
+            autoHideDuration={6000}
+            onClose={closeSnackbar}
+        >
+            <Alert 
+                severity={SnackData.severity} 
+                variant="filled" 
+                action={<Button color="inherit" onClick={closeSnackbar} >close</Button>}
+            >{SnackData.message}</Alert>
+        </Snackbar>
+        </>
     )
 };
 
---
+++ src/pages/contact/ENDPOINT.ts
@@ -0,0 +1,4 @@
+const BASE = "http://localhost:8000/";
+const SEND_MESSAGE = "message/api/random_message/";
+
+export {BASE, SEND_MESSAGE};
