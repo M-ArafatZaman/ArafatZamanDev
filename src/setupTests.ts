daf4811e46ee01b677b56f169a5decb6794e87d5
M-ArafatZaman <mdarafatzaman1@gmail.com>
2023-02-22 17:15:33 -0330

    Mocked the Raspberry PI model

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,5 +2,18 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+import React from 'react';
 import '@testing-library/jest-dom';
 window.scrollTo = jest.fn();
+
+afterAll(() => {
+    jest.clearAllMocks();
+});
+
+// Mock the raspberry pi component
+jest.mock("./pages/home/raspberry", () => {
+    const Fn: React.FC = () => {
+        return React.createElement("span");
+    };
+    return Fn;
+});
