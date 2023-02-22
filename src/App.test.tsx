85a2aaa46efc03501167b0f76f149a7f2cfe9411
M-ArafatZaman <mdarafatzaman1@gmail.com>
2023-02-21 23:53:58 -0330

    Updated test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+// Test that component render
+test('renders', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
 });
