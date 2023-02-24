c08d110afd183736c83330331504a8b8101b8447
M=ArafatZaman <mdarafatzaman1@gmail.com>
2023-02-23 23:48:34 -0330

    Properly structured the tests

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import "jest-canvas-mock";
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '../App';
 
 // Test that component render
 test('renders', () => {
