06a3d0de916668a7b9747ef2ac933c2af3a8e2a7
M=ArafatZaman <mdarafatzaman1@gmail.com>
2023-02-24 23:47:31 -0330

    Added tests for home page

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -4,7 +4,6 @@ import { render, screen } from '@testing-library/react';
 import App from '../App';
 
 // Test that component render
-test('renders', () => {
+test('The whole app renders', () => {
   render(<App />);
-  
 });
---
+++ src/tests/pages/home.test.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {BrowserRouter} from 'react-router-dom';
+import "jest-canvas-mock";
+import {render, screen, waitFor} from '@testing-library/react';
+import Home from '../../pages/home';
+
+
+test("The home page renders correctly", () => {
+    render(
+        <BrowserRouter>
+            <Home/>
+        </BrowserRouter>
+    );
+    
+    // The "VISIT" button for the portfolio sections are rendered correctly
+    waitFor(() => {
+        expect(screen.findByText("VISIT")).toBeTruthy();
+    })
+
+    // Expect InPeril title to be rendered
+    waitFor(() => {
+        expect(screen.getByText("InPeril")).toBeTruthy();
+    })
+
+    // Expect LyricsFinder title to be rendered
+    waitFor(() => {
+        expect(screen.getByText("LyricsFinder")).toBeTruthy();
+    })
+})
