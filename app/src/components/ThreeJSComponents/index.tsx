e7babe18aafdc4f716761881914384d42b007291
M=ArafatZaman <mdarafatzaman1@gmail.com>
2023-03-04 13:30:52 -0330

    Copy pasted the GLTFLoader and OrbitControls source code because remix does not work with natural imports for some reason

---
+++ app/src/components/ThreeJSComponents/index.tsx
@@ -0,0 +1,9 @@
+import {GLTFLoader} from './GLTFLoader';
+import {OrbitControls} from './OrbitControls';
+
+/**
+ * Just copy pasted the GLTFLoader and OrbitControls code from the source
+ * Because for some reason importing from source does not work with Remix
+ */
+
+export {GLTFLoader, OrbitControls};
