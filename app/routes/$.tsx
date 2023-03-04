9db1bf8f1be6679229c7fd4b8800ae35fbf8b15c
M=ArafatZaman <mdarafatzaman1@gmail.com>
2023-03-04 18:35:07 -0330

    Added meta tags for error page

--- app/routes/$.tsx
+++ app/routes/$.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import {MetaFunction} from '@remix-run/node';
+import {MetaTags} from '../src/utils';
 // Error 404 page
 import P404 from '../src/pages/P404';
+
+export const meta: MetaFunction = () => MetaTags({
+    title: "Not found.",
+    description: "Sorry! Could not locate any relevant page.",
+    image: "https://i.imgur.com/orYlTL7.png"
+});
 
 export default P404;
