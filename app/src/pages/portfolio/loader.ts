a7e7827d95d292ac00e83b7107715f5dabad1dee
M=ArafatZaman <mdarafatzaman1@gmail.com>
2023-06-28 23:52:15 -0230

    Debugging mode

--- app/src/pages/portfolio/loader.ts
+++ app/src/pages/portfolio/loader.ts
@@ -12,46 +12,48 @@ export const GET_PORTFOLIO_ITEMS = "projects/api/get_portfolio_items/";
 
 // Fetch function to get portfolio items
 export const GetPortfolioItemsLoader: LoaderFunction = async () => {
-    const data = await prisma.portfolio.findMany({
-        select: {
-            name: true,
-            short_description: true,
-            image: true,
-            tags: true,
-            slug: true,
-        },
-        orderBy: {
-            date_created: "desc"
-        }
-    });
+    // const data = await prisma.portfolio.findMany({
+    //     select: {
+    //         name: true,
+    //         short_description: true,
+    //         image: true,
+    //         tags: true,
+    //         slug: true,
+    //     },
+    //     orderBy: {
+    //         date_created: "desc"
+    //     }
+    // });
 
-    // If nothing is found, an error must have occured
-    if (!data) {
-        return json({
-            status: "Not Found."
-        })
-    };
+    // // If nothing is found, an error must have occured
+    // if (!data) {
+    //     return json({
+    //         status: "Not Found."
+    //     })
+    // };
 
-    // Revert back to getting data from portfolio website
-    const d = await fetch(`${BASE}${GET_PORTFOLIO_ITEMS}`, {
+    // Revert back to getting data from portfolio website]
+    console.log(`${BASE}${GET_PORTFOLIO_ITEMS}`);
+    console.log("http://localhost:8000/projects/api/get_portfolio_items/")
+    const d = await fetch(`http://localhost:8000/projects/api/get_portfolio_items/`, {
         method: "GET",
         mode: "cors"
     });
 
     console.log(d)
 
-    const response: PortfolioAPIResponse = {
-        status: "OK",
-        items: data.map((elem) => ({
-            "name": elem.name as string,
-            "short_description": elem.short_description as string,
-            "imageURL": elem.image as string,
-            "tags": elem.tags?.split(",") as string[],
-            "slug": elem.slug as string
-        }))
-    };
+    // const response: PortfolioAPIResponse = {
+    //     status: "OK",
+    //     items: data.map((elem) => ({
+    //         "name": elem.name as string,
+    //         "short_description": elem.short_description as string,
+    //         "imageURL": elem.image as string,
+    //         "tags": elem.tags?.split(",") as string[],
+    //         "slug": elem.slug as string
+    //     }))
+    // };
 
-    return json(response);
+    return json({status: "Not Found."});
 };
 
 // Fetch function to view an individual portfolio item
